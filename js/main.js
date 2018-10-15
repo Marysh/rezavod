0ddc8f652faaf918ff427714e191319626ee1024
Marysh <35464372+Marysh@users.noreply.github.com>
2018-10-15 12:53:09 +0300

    adaptive

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('.fis_animated-bg').addClass('-animate');
+    // animIndexH1();
+});
+
+// function animIndexH1() {
+//     var arr = ["Атмосферу", "Їжу", "Книги", "Траву", "Їжаків", "Дітей"];
+//     var el = $('.fis_title-right');
+//
+//     var i = 1;
+//     var timerId = setInterval(function () {
+//         el.html(arr[i]);
+//         i++;
+//         if ( i >= arr.length) {
+//             clearInterval(timerId);
+//         }
+//     }, 1000);
+//
+// }
